71da0cd97a661eda7e617abd3c2ec41e405b8be3
KevinGitari <Kevin>
2025-01-10 22:36:20 +0300

    first commit

---
+++ react-app/src/assets/Message.tsx
@@ -0,0 +1,13 @@
+
+// PascalCasing-Capitalizing the second word
+function Message () {
+
+    const name = ''
+    if (name)
+
+    return <h1>Hello {name}</h1>;
+    return <h1>Hello World</h1>;
+
+}
+
+export default Message;
